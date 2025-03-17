f64993375c6780dd7753f4cd5728145b4681a698
Pol Piñol Castuera <pol.pinolcastuera@gmail.com>
2025-03-17 19:53:09 +0100

    feat: add initial structure for CI (#2)

--- src/test/java/io/autoinvestor/AlertsApplicationTests.java
+++ src/test/java/io/autoinvestor/AlertsApplicationTests.java
@@ -1,4 +1,4 @@
-package io.autoinvestor.alerts;
+package io.autoinvestor;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AlertsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
