4b94bf574fb71be5aba4abe1d41774f4ec6e776d
Álvaro Mañoso Oca <43934777+alvaromanoso@users.noreply.github.com>
2025-04-21 18:20:22 +0200

    Error handling 2 (#11)

--- src/main/java/io/autoinvestor/adapter/pubsub/user/UserEventPubSubInboundAdapter.java
+++ src/main/java/io/autoinvestor/adapter/pubsub/user/UserEventPubSubInboundAdapter.java
@@ -11,9 +11,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.channel.PublishSubscribeChannel;
-import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.handler.annotation.Header;
 
@@ -32,21 +30,14 @@ class UserEventPubSubInboundAdapter {
         return new PublishSubscribeChannel();
     }
 
-    @Bean("errorChannel")
-    public MessageChannel errorChannel() {
-        return new DirectChannel();
-    }
-
     @Bean
     public PubSubInboundChannelAdapter inboundChannelAdapter(
             @Qualifier(PUBSUB_CHANNEL) MessageChannel messageChannel,
-            @Qualifier("errorChannel") MessageChannel errorChannel,
             PubSubTemplate pubSubTemplate
     ) {
         PubSubInboundChannelAdapter adapter = new PubSubInboundChannelAdapter(pubSubTemplate, PUBSUB_CHANNEL);
         adapter.setOutputChannel(messageChannel);
-        adapter.setErrorChannel(errorChannel);
-        adapter.setAckMode(AckMode.MANUAL);
+        adapter.setAckMode(AckMode.AUTO);
         adapter.setPayloadType(UserEvent.class);
         return adapter;
     }
@@ -63,19 +54,5 @@ class UserEventPubSubInboundAdapter {
             case "USER_UPDATED" -> userUpdatedEventProcessor.process((UserUpdatedEvent) payload);
             default -> log.error("Unrecognized event type: {}", payload.type());
         }
-        
-        message.ack().thenRun(() -> log.info("Message acknowledged (ID: {})", message.getPubsubMessage().getMessageId()));
-    }
-
-    @ServiceActivator(inputChannel = "errorChannel")
-    public void handleError(
-            Message<?> errorMessage,
-            @Header(GcpPubSubHeaders.ORIGINAL_MESSAGE) BasicAcknowledgeablePubsubMessage message
-    ) {
-        var errorMessagePayload = errorMessage.getPayload();
-        var originalPayload = message.getPubsubMessage().getData().toStringUtf8();
-        log.info("Received errored message (ID: {}). Error message {}. Original payload: {}", message.getPubsubMessage().getMessageId(), errorMessagePayload, originalPayload);
-
-        message.ack().thenRun(() -> log.info("Error message acknowledged (ID: {})", message.getPubsubMessage().getMessageId()));
     }
 }
